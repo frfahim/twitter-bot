a5e5ec7b70ca8dc35c56989e984a50c99dabe5d4
Farhadur Raja Fahim <FarhadurFahim@users.noreply.github.com>
2017-01-19 00:30:37 +0600

    Add update status

---
+++ update_status_bot.py
@@ -0,0 +1,11 @@
+import tweepy
+
+#import twitter apps configaration file from config.py file
+from config import *
+
+#Authentication using keys & accesstoken
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+api.update_status(status="This is a sample tweet using Tweepy with python")
