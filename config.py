dc389936c7e6e03fff8803cde3ba88064e618ed0
Farhadur Raja Fahim <FarhadurFahim@users.noreply.github.com>
2017-01-19 14:55:14 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-consumer_key = "071gEopCXVtaLJGZZMAb7VRJ8"
-consumer_secret = "im17lsN8LAHi8DH5pibE9w7m2SxEVzupPq2ji8qV4Cv389rtsN"
+consumer_key = "Your consumer key"
+consumer_secret = "Your consumer secret"
 
-access_token = "2545898138-WBm8wOrk7xyi6SA4Qt7WWBrzlyNlwFkasrTmycg"
-access_token_secret = "dn5UdIty9IV8fumxJSLzKObES7emCGahs1vzrLrqf0Og5"
+access_token = "Your access token"
+access_token_secret = "Your access token secret"
